5434e5d6af53979f4a4384a867b7af05607e1f3d
RahulMourya <rahulmourya336@gmail.com>
2018-08-06 15:19:34 +0530

    "Add Manage Minor Update"

--- update_trip_expense.php
+++ update_trip_expense.php
@@ -18,7 +18,7 @@ $expense_amount = $_POST['expense_amount'];
 $trip_id = mysqli_real_escape_string($connection, trim($_GET['t_id']));
 $user_id = $_SESSION['current_user_id'];
 
-$update_query = "UPDATE trip_expenese SET  t_id = '$trip_id', u_id = '$user_id', ex_name = '$expense_name', ex_date = '$expense_date', ex_amount = '$expense_amount' where ex_id = '$expense_id'";
+$update_query = "UPDATE trip_expenese SET ex_name = '$expense_name', ex_date = '$expense_date', ex_amount = '$expense_amount', c_id = '$expense_category' where ex_id = '$expense_id'";
 
 //print($trip_id." ".$trip_name ." ". $trip_url ."". $trip_start_date."". $trip_end_date );
 
